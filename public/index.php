4a7522378ff7c98bd8d10cc67efbadbbdd2bfe01
kirubel <kiztopia.2020@gmail.com>
2022-01-30 12:42:15 +0300

    new: fucntion for list all items created

--- public/index.php
+++ public/index.php
@@ -10,30 +10,37 @@
 </head>
 <body>
     <?php include(SHARED_PATH. '/nav.php')?>
+    <?php $items = find_all_items();
+        $count = mysqli_num_rows($items);
+        echo $count;
+        
+    ?>
     <div class="main">
         <div class="items">
-        <div class="item">
-                <a href="">
-                    <h4>dell insprion</h4>
-                </a>
-            </div>
-            <div class="item">
-                <a href="">
-                    <h4>dell 403x</h4>
-                </a>
-            </div>
-            <div class="item">
-                <a href="">
-                    <h4>lenovo think pad</h4>
-                </a>
-            </div>
-            <div class="item">
-                <a href="">
-                    <h4>acer 693p</h4>
-                </a>
-            </div>
+            <table border="1">
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>manufacturer</th>
+                    <th>ram</th>
+                </tr>
+                <?php 
+                
+                
+                while($item = mysqli_fetch_assoc($items)){ ?>
+                    <tr>
+                        
+                    <td><a href=""><?php echo $item['id'] ?></a></td>
+                    <td><?php echo $item['name'] ?></td>
+                    <td><?php echo $item['manufacturer'] ?></td>
+                    <td><?php echo $item['ram'] ?></td>
+                        
+                    </tr>
+                <?php } ?>
+                
 
-        </div>
+            </table>
+        
     </div>
 </body>
 </html>
